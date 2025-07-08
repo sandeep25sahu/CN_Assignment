e67724245052cb2d071662ca522bc46587cce5a6
sandeep25sahu <www.sndeep9009@gmail.com>
2025-07-09 00:54:53 +0530

    responsive

--- src/components/ProductCard/ProductCard.vue
+++ src/components/ProductCard/ProductCard.vue
@@ -130,7 +130,7 @@ const filteredProducts = computed(() => {
     </div>
 
     <!-- Product Grid -->
-    <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
+    <div class="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3  gap-6">
       <div
         v-for="product in filteredProducts"
         :key="product.id"
